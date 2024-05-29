7f138941feee0b9edbcef9624d5dd080b835b814
Franzie Yoogan <franzieyoogan@gmail.com>
2024-05-29 19:40:54 -0300

    api

--- .venv/Scripts/app.py
+++ .venv/Scripts/app.py
@@ -1,14 +1,31 @@
+import json
 from flask import Flask, render_template, request
 from newspaper import Article
+import requests 
 from lxml_html_clean import Cleaner
+from bs4 import BeautifulSoup 
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+  
+  response = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=019145f2a2e1437980e54a6154024cbc")
 
- return render_template('home.htm')
+  data = response.text
+
+  parseJson = json.loads(data)
+
+  dataArticle = parseJson['articles']
+  print(dataArticle)
+
+
+  return render_template('home.htm',dataArticle = dataArticle)
+
+  
+
+
 
 @app.route('/search', methods=['GET','POST'])
 def Search():
